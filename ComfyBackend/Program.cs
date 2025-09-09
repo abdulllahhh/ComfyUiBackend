ec3e11f958f92165d13efeeb0706b0e1301d8754
abdullah <abdullahibrahim.business@gmail.com>
2025-09-09 09:44:37 +0300

    remove duplicate bearer

--- ComfyBackend/Program.cs
+++ ComfyBackend/Program.cs
@@ -74,20 +74,6 @@ namespace ComfyBackend
             var key = Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]);
             
             
-            builder.Services.AddAuthentication("Bearer")
-                .AddJwtBearer("Bearer", options =>
-                {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ValidateLifetime = true,
-                        ValidateIssuerSigningKey = true,
-                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-                        ValidAudience = builder.Configuration["Jwt:Audience"],
-                        IssuerSigningKey = new SymmetricSecurityKey(key)
-                    };
-                });
             
             builder.Services.AddAuthorization();
             builder.Services.AddControllers();
